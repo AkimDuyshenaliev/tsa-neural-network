28c4e84a3a1fd4c1f84dfa42b4ee75b98fb7bf49
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-03-28 12:40:37 +0300

    Complete csv cleaning function

--- dataCleaning.py
+++ dataCleaning.py
@@ -6,6 +6,7 @@ def csvCleaning(data):
     df.drop_duplicates(inplace=True) # Remove duplicates in the original DataFrame
     df.dropna(inplace=True)
     df = df[df['comment'].str.contains('.desktop') == False]
-    df = df[df['comment'].str.encode('UTF-8', errors='ignore').str.decode('UTF-8')]
+    df['comment'] = df['comment'].str.replace('\n', '')
+    df['comment'] = df['comment'].str.replace('\t', '')
+    df['comment'] = df['comment'].str.replace('[^\w\s]', '')
     df.to_csv('data/clean_data.csv')
-    print(df)
