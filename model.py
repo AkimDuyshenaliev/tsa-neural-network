a1383608e20fa1f1479eb4c89acc61bfba650e14
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-03-30 12:39:15 +0300

    Reorganized imports

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow as ts
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
 
 
 def text_sentiment_neural_network(testData, trainData):
