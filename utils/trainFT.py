a8d6d263a975bc8deb20c1803e8da70337d4fc42
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-04-19 15:06:17 +0300

    A simple unsupervised train model

--- utils/trainFT.py
+++ utils/trainFT.py
@@ -2,6 +2,16 @@ import pandas as pd
 import fasttext
 from tqdm import tqdm
 
+from utils.utils import coloring
 
-def trainFastText():
-    pass
+
+def trainFastText(commentData, vocabData):
+    model = fasttext.train_unsupervised('data/forFastText/tempData.csv', epoch=12, thread=4)
+    model.save_model('data/test_model.bin')
+
+    coloring(data='Model trained and saved', r='38', g='05', b='46')
+
+
+def readFastTextModel(model):
+    model = fasttext.load_model(model)
+    print(model.words)
