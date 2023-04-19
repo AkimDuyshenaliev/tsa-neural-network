ba28f4ca335a990e36016f4d6b9a449d8d4b3132
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-04-19 15:06:35 +0300

    Binary search

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,18 @@
 
 
+def binSearch(data, search):
+    while True:
+        listMid = (len(data)-1)//2
+        if search == data[listMid]:
+            return listMid
+        elif search > data[listMid]:
+            del data[:listMid+1]
+            continue
+        elif search < data[listMid]:
+            del data[listMid+1:]
+            continue
+
+
 def color(r="38", g="05", b="222"):
     def my_docorator(func):
         def wrapper(self=None):
