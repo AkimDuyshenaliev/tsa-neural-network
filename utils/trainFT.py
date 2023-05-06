75fb4916967ed119386a5a80a70da34b5d7ee9c6
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-05-06 12:59:02 +0300

    Fast text supervised training

--- utils/trainFT.py
+++ utils/trainFT.py
@@ -5,15 +5,17 @@ from utils.utils import coloring
 
 
 def trainFastText(data):
-    model = fasttext.train_unsupervised(data, epoch=4, thread=4)
-    model.save_model('data/test_model.bin')
+    # model = fasttext.train_unsupervised(data, epoch=4, thread=4)
+    model = fasttext.train_supervised('data/ft_training_data.txt', epoch=25, wordNgrams=2)
+    model.save_model('data/supervised_ft_model.bin')
 
     coloring(data='Model trained and saved', r='38', g='05', b='46')
 
 
 def readFastTextModel(model):
-    model = fasttext.load_model(model)
+    model = fasttext.load_model('data/supervised_ft_model.bin')
     # print(model.get_nearest_neighbors(str(input('Write a word: '))))
-    sentence = '_'
-    sentence = sentence.split(sep=' ')
-    [print(model.get_word_vector(word)) for word in sentence]
+    # sentence = sentence.split(sep=' ')
+    # [print(model.get_word_vector(word)) for word in sentence]
+    while True:
+        print(model.predict(str(input('Write a sentence: '))))
