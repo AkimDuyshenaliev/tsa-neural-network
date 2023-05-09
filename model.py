774f378c6fce16eb2809365809e60e4a5a4b643b
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-05-09 17:18:10 +0300

    Changes to models

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.activations import softmax
 
 from utils.utils import data_preprocessing, draw_plt
 
@@ -14,18 +15,22 @@ def rnn_model(data, tweetsData, ftModelData):
         tweetsData=tweetsData,
         ftModelData=ftModelData)
 
+    afunc = 'tanh'
+    samples = 16
     model = keras.Sequential()
+
     if ftEmbedding == 0:
-        print('ftEmbbedding')
         model.add(layers.Embedding(10000, 64, input_length=10))
     else:
         model.add(layers.InputLayer(input_shape=x_valid.shape[1:]))
-    model.add(layers.LSTM(128))
+
+    model.add(layers.Bidirectional(layers.LSTM(samples*2, return_sequences=True, activation=afunc)))
+    model.add(layers.Bidirectional(layers.LSTM(samples*2, activation=afunc)))
     model.add(layers.Dense(10))
     model.summary()
 
     model.compile(
-        optimizer='sgd',
+        optimizer='Adam',
         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
 
@@ -46,26 +51,27 @@ def ft_cnn_model(data, tweetsData, ftModelData):
         ftModelData=ftModelData) 
 
     ## CNN Model ###
+    afunc = 'relu'
     samples = 16
     model = keras.models.Sequential()
     if ftEmbedding == 0:
         model.add(layers.Embedding(10000, 64, input_length=10))
-    model.add(layers.Conv1D(samples, 3, activation='relu', input_shape=(10, 100), padding='same'))
+    model.add(layers.Conv1D(samples, 3, activation=afunc, input_shape=(10, 100), padding='same'))
     model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling1D())
-    model.add(layers.Conv1D((samples*2), 3, activation='relu', padding='same'))
+    model.add(layers.Conv1D((samples*2), 3, activation=afunc, padding='same'))
     model.add(layers.BatchNormalization())
     model.add(layers.MaxPooling1D())
-    model.add(layers.Conv1D((samples*4), 3, activation='relu', padding='same'))
+    model.add(layers.Conv1D((samples*4), 3, activation=afunc, padding='same'))
 
     model.add(layers.Flatten())
-    model.add(layers.Dense((samples*8), activation='relu'))
+    model.add(layers.Dense((samples*8), activation=afunc))
     model.add(layers.Dense(10))
 
     model.summary()
 
     model.compile(
-        optimizer='adam',
+        optimizer='Adam',
         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
 
